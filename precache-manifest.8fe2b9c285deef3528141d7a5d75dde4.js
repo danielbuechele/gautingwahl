643b5fa54ba3184f70e51a39f1d1ce68b53a3cd3
Daniel Büchele <daniel@buechele.cc>
2020-02-23 23:26:04 +0000

    deploy

---
+++ precache-manifest.8fe2b9c285deef3528141d7a5d75dde4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6c52642d0c9060acb2ba3ecb36bb9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4ee4853f5fb3772016d",
+    "url": "/static/css/main.735ca646.chunk.css"
+  },
+  {
+    "revision": "b0a867533fbcbb732c30",
+    "url": "/static/js/2.71c2137b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.71c2137b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4ee4853f5fb3772016d",
+    "url": "/static/js/main.11b8fab4.chunk.js"
+  },
+  {
+    "revision": "1ce1fc6567b34a4a9fde",
+    "url": "/static/js/runtime-main.54cba194.js"
+  }
+]);
--- src/useIsMounted.ts
+++
@@ -1,9 +0,0 @@
-import { useState, useEffect } from "react";
-
-export default function useIsMounted() {
-  const [isMounted, setIsMounted] = useState(false);
-  useEffect(() => {
-    setTimeout(() => setIsMounted(true), 50);
-  }, []);
-  return isMounted;
-}
