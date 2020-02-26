359da7595497326b13daa66d701e187ac37bb21c
Daniel Büchele <daniel@buechele.cc>
2020-02-26 00:51:41 +0000

    fix build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -4,6 +4,8 @@ import path from "path";
 import { Data, Party, Answer, Tag, AnswerGroup } from "../src/types";
 const GOOGLE_SHEET = "1NDsL-MS7z2kELEdHZaEFDNz27mOlyecTVxkGVF6bASI";
 
+type T = keyof typeof Tag;
+
 (async () => {
   if (!process.env.REACT_APP_GOOGLE_API_KEY) {
     throw new Error("REACT_APP_GOOGLE_API_KEY is not set as env var");
@@ -13,34 +15,38 @@ const GOOGLE_SHEET = "1NDsL-MS7z2kELEdHZaEFDNz27mOlyecTVxkGVF6bASI";
   ).then(res => res.json());
 
   const data: Data = {
-    questions: result.values.slice(1).map(([q, tags, ...rest]: string[]) => {
-      const answers = Object.values(Party).reduce((acc, party, i) => {
-        let explanation = rest[i] || "=";
-        let answer = Answer.NEUTRAL;
-        if (explanation[0] === "+") {
-          answer = Answer.POSITIVE;
-        } else if (explanation[0] === "-") {
-          answer = Answer.NEGATIVE;
-        } else if (explanation[0] === "=") {
-          answer = Answer.NEUTRAL;
-        } else {
-          throw new Error(`Wrong answer at ${party} question ${i}`);
-        }
-        explanation = explanation.substring(1).trim();
+    questions: result.values
+      .slice(1)
+      .map(([q, tags = "", ...rest]: string[]) => {
+        const answers = Object.values(Party).reduce((acc, party, i) => {
+          let explanation = rest[i] || "=";
+          let answer = Answer.NEUTRAL;
+          if (explanation[0] === "+") {
+            answer = Answer.POSITIVE;
+          } else if (explanation[0] === "-") {
+            answer = Answer.NEGATIVE;
+          } else if (explanation[0] === "=") {
+            answer = Answer.NEUTRAL;
+          } else {
+            throw new Error(`Wrong answer at ${party} question ${i}`);
+          }
+          explanation = explanation.substring(1).trim();
 
-        acc[party] = {
-          answer,
-          explanation
+          acc[party] = {
+            answer,
+            explanation
+          };
+          return acc;
+        }, {} as AnswerGroup);
+
+        const cleanTags = tags.split(",").map(s => s.trim()) as T[];
+
+        return {
+          question: q,
+          tags: cleanTags.map(t => Tag[t]),
+          answers
         };
-        return acc;
-      }, {} as AnswerGroup);
-
-      return {
-        question: q,
-        tags: tags.split(",").map(t => Tag[t.trim()]),
-        answers
-      };
-    })
+      })
   };
 
   fs.writeJSON(path.join(__dirname, "..", "src", "data.json"), data);
